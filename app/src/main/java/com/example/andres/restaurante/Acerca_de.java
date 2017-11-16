f2ed629a0f3d064c5120c04c8ee6856fb408b0d1
Andreso07 <carlos_guerrerob@hotmail.com>
2017-11-16 10:56:19 -0500

    en progreso

---
+++ app/src/main/java/com/example/andres/restaurante/Acerca_de.java
@@ -0,0 +1,22 @@
+package com.example.andres.restaurante;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by andres on 11/15/17.
+ */
+
+public class Acerca_de extends Activity {
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.acerca_de, container, false);
+    }
+
+
+}
